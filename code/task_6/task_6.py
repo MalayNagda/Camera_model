17daa393c926e7893b1c49016913765e7f3e5179
MalayNagda <35292631+MalayNagda@users.noreply.github.com>
2020-05-27 18:07:22 -0700

    commit 3

--- code/task_6/task_6.py
+++ code/task_6/task_6.py
@@ -7,9 +7,22 @@ from cv2 import aruco
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import axes3d, Axes3D
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection, Line3DCollection
+import csv
 
-mtx_left = np.array([[423.27381306, 0, 341.34626532], [0, 421.27401756, 269.28542111], [0, 0, 1]])
-dist_left = np.array([-0.43394157423038077, 0.26707717557547866, -0.00031144347020293427, 0.0005638938101488364, -0.10970452266148858])
+def camera_params(filename):
+    params=[]
+    with open(filename, 'r') as f:
+        reader = csv.reader(f)
+        # read file row by row
+        for row in reader:
+            params.append(row)
+    return params
+
+params= camera_params('../../parameters/left_camera_intrinsics.csv')
+left_intrinsics=np.reshape([params[1],params[2],params[3]],(3,3))
+dist1=np.reshape([params[5]],(1,5))
+left_intrinsics=left_intrinsics.astype('float64')
+dist1=dist1.astype('float64')
 
 translations = []
 rotations = []
@@ -20,61 +33,60 @@ fig = plt.figure()
 plt.axis('equal')
 ax = fig.add_subplot(111, projection='3d')
 for i in range(11):
-	img = cv2.imread("../../images//task_6//left_" + str(i) + ".png")
-	aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_250)
-	corners, ids, rejectedImgPoints = aruco.detectMarkers(img, aruco_dict)
-
-	# print("corners", corners)
-
-	img_marker = aruco.drawDetectedMarkers(img, corners, ids)
-	cv2.imshow("Detecting Marker on image " + str(i), img_marker)
-	cv2.waitKey(0)
-	cv2.imwrite("../../output/task_6/Marker on image left_" + str(i) + ".png",img_marker)
+    img = cv2.imread("../../images//task_6//left_" + str(i) + ".png")
+    aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_250)
+    corners, ids, rejectedImgPoints = aruco.detectMarkers(img, aruco_dict)
+    
+    cnr = np.asarray(corners[0][0])
+    csv_file_name = '../../parameters/left_'+str(i)  # getting the name of the file
+    np.savetxt('{}.csv'.format(csv_file_name), cnr, delimiter=',', fmt='%f')
+    
+    img_marker = aruco.drawDetectedMarkers(img, corners, ids)
+    cv2.imshow("Detecting Marker on image " + str(i), img_marker)
+    cv2.waitKey(0)
+    cv2.imwrite("../../output/task_6/Marker on image left_" + str(i) + ".png",img_marker)
 
 	# img_corners = np.float32(corners).reshape(-1,2)
-	objectpoints = np.array([[0,0,0], [1,0,0], [1,1,0], [0,1,0]], dtype=np.float32)
+    objectpoints = np.array([[0,0,0], [1,0,0], [1,1,0], [0,1,0]], dtype=np.float32)
 	# img_points = np.array([[0,0], [1,0], [1,1], [0,1]], dtype=np.float32)
-	retval, rvec, tvec = cv2.solvePnP(objectpoints, corners[0][0], mtx_left, dist_left)
+    retval, rvec, tvec = cv2.solvePnP(objectpoints, corners[0][0], left_intrinsics, dist1)
 
-	# print("rvec", rvec)
-	# print("tvec", tvec)
+    tvec = 5 * tvec #Scaling 
 
-	tvec = 5 * tvec #Scaling 
-
-	translations.append(tvec)
-	rotations.append(rvec)
+    translations.append(tvec)
+    rotations.append(rvec)
 
 	# Converting rotation vector to matrix
-	rotM, a = cv2.Rodrigues(rvec)
-	camera_position = -np.matrix(rotM).T * np.matrix(tvec)
+    rotM, a = cv2.Rodrigues(rvec)
+    camera_position = -np.matrix(rotM).T * np.matrix(tvec)
 
-	rotation_vec.append(rotM)
+    rotation_vec.append(rotM)
 
-	xs = camera_position[0] # elements of np matrix
-	ys = camera_position[1]
-	zs = camera_position[2]
+    xs = camera_position[0] # elements of np matrix
+    ys = camera_position[1]
+    zs = camera_position[2]
 
-	ax.scatter(xs, ys, zs, c='b', marker='^', s = 0, zdir = 'z')
+    ax.scatter(xs, ys, zs, c='b', marker='^', s = 0, zdir = 'z')
+    ax.text(xs[0,0] + 0.01, ys[0,0] + 0.01, zs[0,0], 'left_'+str(i)+'.png', zdir='y')
+    positions.append(camera_position)
 
-	positions.append(camera_position)
-
-	print("\n Image left_"+ str(i))
-	print("R : ", rotM.T)
-	print("\n T : ", camera_position)
-	print("\n")
+    print("\n Image left_"+ str(i))
+    print("R : ", rotM.T)
+    print("\n T : ", camera_position)
+    print("\n")
 
 
 	## Code to draw pyramid
-	vert = np.array([[-1, 1, 1, -1, 0], [-1, -1, 1, 1, 0], [1, 1, 1, 1, 0], [1, 1, 1, 1, 1]])
-	points = np.hstack((rotM.T, camera_position))
-	points = np.append(points, [[0, 0, 0, 1]], axis=0)
+    vert = np.array([[-1, 1, 1, -1, 0], [-1, -1, 1, 1, 0], [1, 1, 1, 1, 0], [1, 1, 1, 1, 1]])
+    points = np.hstack((rotM.T, camera_position))
+    points = np.append(points, [[0, 0, 0, 1]], axis=0)
 	# print(points)
-	vert_mul = (np.matrix(points) * np.matrix(vert))
-	vert_mul = np.delete(vert_mul, 3, 0)
-	vert_points = np.array(vert_mul.T)
-	vertices = [[vert_points[0], vert_points[1], vert_points[4]], [vert_points[0], vert_points[3], vert_points[4]], [vert_points[2], vert_points[1], vert_points[4]], [vert_points[2], vert_points[3], vert_points[4]], [vert_points[0], vert_points[1], vert_points[2], vert_points[3]]]
+    vert_mul = (np.matrix(points) * np.matrix(vert))
+    vert_mul = np.delete(vert_mul, 3, 0)
+    vert_points = np.array(vert_mul.T)
+    vertices = [[vert_points[0], vert_points[1], vert_points[4]], [vert_points[0], vert_points[3], vert_points[4]], [vert_points[2], vert_points[1], vert_points[4]], [vert_points[2], vert_points[3], vert_points[4]], [vert_points[0], vert_points[1], vert_points[2], vert_points[3]]]
 	# print(vertices)
-	ax.add_collection3d(Poly3DCollection(vertices, facecolors='white', linewidths=1, edgecolors='k', alpha=.25))
+    ax.add_collection3d(Poly3DCollection(vertices, facecolors='white', linewidths=1, edgecolors='k', alpha=.25))
 
 
 ## Plot for square with red dot at the corner in 3D
@@ -98,21 +110,3 @@ ax.set_ylabel('Y axis')
 ax.set_zlabel('Z axis')
 plt.show()
 plt.savefig('../../output/task_6/camera_pose.png',dpi=600, bbox_inches = "tight")
-    # break
-	# plot_pyramid(ax, R.T, camera_position)
-	# print(cameraPosition[0][0])
-	# print(tvec.shape)
-	# plot_pyramid(ax, R.T, camera_position)
-# plot_square(ax)
-# ax.set_xlim(-15, 15)
-# ax.set_ylim(15,-15)
-# ax.set_zlim(25,-25)
-# ax.set_xlabel('X axis')
-# ax.set_ylabel('Y axis')
-# ax.set_zlabel('Z axis')
-
-
-# cv2.destroyAllWindows()
-# plt.show()
-
-
--- code/task_7/task7.py
+++ code/task_7/task7.py
@@ -1,39 +1,21 @@
-import numpy as np
-import cv2
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-import csv
-from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 import argparse
 from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
 
-fx = 423.27381306
-fy = 421.27401756
-cx = 341.34626532
-cy = 269.28542111
+import os
+import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
+import math
+import numpy as np
+import cv2
+import glob
+import csv
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 
-K = np.float64([[fx, 0, cx], 
-                [0, fy, cy], 
-                [0, 0, 1]])
-
-D = np.float64([-0.43394157423038077, 0.26707717557547866, -0.00031144347020293427, 0.0005638938101488364,-0.10970452266148858])
-
-img1 = cv2.imread('../../images/task_7/left_4.png',0)
-
-img2 = cv2.imread('../../images/task_7/left_5.png',0)
-
-objp = np.zeros((6*9,3), np.float32)
-objp[:,:2] = np.mgrid[0:9,0:6].T.reshape(-1,2)
-objpoints = []
-imagepoints = []
-objpoints.append(objp)
-objpoints = np.float64(objpoints)
-
-T = np.float64([[-2.45477982343564],
-                [-0.0332453385522794],
-                [-0.0198820833667838]])
-
-T = np.transpose(T)
+#offset matrix
+offset_matrix = np.array([[1, 0,      -1],
+ [  0,     1, -1],
+ [  0,          0,          1        ]])
 
 def camera_params(filename):
     params=[]
@@ -44,74 +26,166 @@ def camera_params(filename):
             params.append(row)
     return params
 
-params=camera_params('../../parameters/stereo_calibration.csv')
-R=np.reshape([params[1],params[2],params[3]],(3,3))
-#T=np.reshape([params[5],params[6],params[7]],(3,1))
-E=np.reshape([params[9],params[10],params[11]],(3,3))
-F=np.reshape([params[13],params[14],params[15]],(3,3))
-R=R.astype('float64')
-#T=T.astype('float64')
-E=E.astype('float64')
-F=F.astype('float64')
+params= camera_params('../../parameters/left_camera_intrinsics.csv')
+left_intrinsics=np.reshape([params[1],params[2],params[3]],(3,3))
+dist1=np.reshape([params[5]],(1,5))
+left_intrinsics= left_intrinsics.astype('float64')
+dist1=np.asarray(dist1.astype('float64'))
 
-h,  w = img1.shape[:2]
-newcameramtx, roi=cv2.getOptimalNewCameraMatrix(K,D,(w,h),1,(w,h))
-mapx,mapy = cv2.initUndistortRectifyMap(K,D,None,None,(w,h),cv2.CV_32FC1)
-img1 = cv2.remap(img1,mapx,mapy,cv2.INTER_LINEAR)
+params= camera_params('../../parameters/right_camera_intrinsics.csv')
+right_intrinsics=np.reshape([params[1],params[2],params[3]],(3,3))
+dist2=np.reshape([params[5]],(1,5))
+right_intrinsics=right_intrinsics.astype('float64')
+dist2=dist2.astype('float64')
 
-h2,  w2 = img2.shape[:2]
-newcameramtx, roi=cv2.getOptimalNewCameraMatrix(K,D,(w2,h2),1,(w2,h2))
-mapx2,mapy2 = cv2.initUndistortRectifyMap(K,D,None,None,(w2,h2),cv2.CV_32FC1)
-img2 = cv2.remap(img2,mapx2,mapy2,cv2.INTER_LINEAR)
+#This method to undistort the images
+def image_undistort(img, mtx, dist):
+ h, w = img.shape[:2]
+ newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, (w, h), 1, (w, h))
+ #undistoting images
+ dst = cv2.undistort(img, mtx, dist, None, mtx)
+ return dst
 
+def right_angle_distance(img_pt_1, img_pt_2):
+ return math.sqrt((img_pt_2[0] - img_pt_1[0]) ** 2 + (img_pt_2[1] - img_pt_1[1]) ** 2)
 
-for i in range(len(objpoints)):
-    imgpts, _ = cv2.projectPoints(objpoints[i], R[i], T[i], K, D)
-    imagepoints.append(imgpts)
+def orb(img):
+    orb = cv2.ORB_create()
+    kps = orb.detect(img,None)
+    kps, des = orb.compute(img, kps)
+    return kps,des
 
-#print(imagepoints)
+def plot_pyramid(axis, R, T):
+ #print ("plotting pyramid")
+ v = np.array([[-1, -1, 1], [1, -1, 1], [1, 1, 1], [-1, 1, 1], [0, 0, 0]])
+ v = v.T
+ v = np.append(v, [[1, 1, 1, 1, 1]], axis=0)
+ H = np.hstack((R, T))
+ H = np.append(H, [[0, 0, 0, 1]], axis=0)
+ v_t = (np.matrix(H) * np.matrix(v))
+ v_t = np.delete(v_t, 3, 0)
+ v = np.array(v_t.T)*3
 
-gr1 = img1
-gr2= img2
-orb = cv2.ORB_create()
+ verts = [[v[0], v[1], v[4]], [v[0], v[3], v[4]],
+          [v[2], v[1], v[4]], [v[2], v[3], v[4]], [v[0], v[1], v[2], v[3]]]
 
-kp1, des1 = orb.detectAndCompute(img1,None)
-kp2, des2 = orb.detectAndCompute(img2,None)
+ axis.add_collection3d(Poly3DCollection(verts,
+                                        facecolors='white', linewidths=1, edgecolors='k', alpha=.25))
+
+#left and right images
+left_image1=cv2.imread("../../images/task_7/left_1.png")
+left_image1 = cv2.cvtColor(left_image1, cv2.COLOR_BGR2GRAY)
+
+left_image2 = cv2.imread("../../images/task_7/left_3.png")
+left_image2 = cv2.cvtColor(left_image2, cv2.COLOR_BGR2GRAY)
+
+#distort the images
+img1 = image_undistort(left_image1, left_intrinsics, dist1)
+img2 = image_undistort(left_image2, left_intrinsics, dist1)
+
+kp1, des1 = orb(img1)
+kp2, des2 = orb(img2)
 
 bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+matches = bf.match(des1, des2)
 
-matches = bf.match(des1,des2)
+matches = sorted(matches, key=lambda x: x.distance)
+img3 = cv2.drawMatches(img1, kp1, img2, kp2, matches, None,
+                      flags=2)
 
-img3 = cv2.drawMatches(img1,kp1,img2,kp2,matches[:200],None, flags=2)
+cv2.imshow('img3',img3)
+cv2.imwrite("../../output/task_7/Match.jpg",img3)
 
-plt.imshow(img3)
-plt.savefig('../../output/task_7/Figure3.png',dpi=600, bbox_inches = "tight")
-plt.show()
+cv2.waitKey(1000)
+cv2.destroyAllWindows()
 
+left_image_points = np.float32([kp1[m.queryIdx].pt for m in matches]).reshape(-1,2)
 
-
+right_image_points = np.float32([kp2[m.queryIdx].pt for m in matches]).reshape(-1,2)
 
 kp1_match = np.array([kp1[mat.queryIdx].pt for mat in matches])
 kp2_match = np.array([kp2[mat.trainIdx].pt for mat in matches])
 
-kp1_match_ud = cv2.undistortPoints(np.expand_dims(kp1_match,axis=1),K,D)
-kp2_match_ud = cv2.undistortPoints(np.expand_dims(kp2_match,axis=1),K,D)
+kp1_match_ud = cv2.undistortPoints(np.expand_dims(kp1_match, axis=1), left_intrinsics, dist1)
+kp2_match_ud = cv2.undistortPoints(np.expand_dims(kp2_match, axis=1), left_intrinsics, dist1)
 
-E, mask_e = cv2.findEssentialMat(kp1_match_ud, kp2_match_ud, focal=1.0, pp=(0., 0.), 
+E, mask_e = cv2.findEssentialMat(kp1_match_ud, kp2_match_ud, focal=1.0, pp=(0., 0.),
                                method=cv2.RANSAC, prob=0.999, threshold=0.001)
-print("The calculated essential matrix is",E)
-#print ("Essential matrix: used ",np.sum(mask_e) ," of total ",len(matches),"matches")
 
-points, R, t, mask_RP = cv2.recoverPose(E, kp1_match_ud, kp2_match_ud, mask=mask_e)
-#print("points:",points,"\trecover pose mask:",np.sum(mask_RP!=0))
-print("R:",R,"t:",t.T)
+mask_bool=mask_e.astype(bool)
 
-bool_mask = mask_RP.astype(bool)
-img_valid = cv2.drawMatches(img1,kp1,img2,kp2,matches, None, 
-                            matchColor=(0, 255, 0), 
-                            matchesMask=bool_mask.ravel().tolist(), flags=2)
+imgx = cv2.drawMatches(img1, kp1, img2, kp2, matches, None,
+                      matchesMask=mask_bool.ravel().tolist(),flags=2)
 
-plt.imshow(img_valid)
-plt.savefig('../../output/task_7/Figure4.png',dpi=600, bbox_inches = "tight")
+cv2.imshow("inliners", imgx)
+cv2.imwrite("../../output/task_7/Inliners.jpg",imgx)
+cv2.waitKey(1000)
+cv2.destroyAllWindows()
+
+#Recover the required pose
+points, R, t, mask1 = cv2.recoverPose(E, kp1_match_ud, kp2_match_ud)
+
+ra=R+2.5
+ta=t+2.5
+rx=0.5-R
+tx=2-t
+
+mx= np.identity(3)
+mi= np.array([1,2,1])
+mi1=mi.transpose().reshape((3,1))
+
+#Create the two rotational matrices
+#Appending unit matrix and [0,0,0]
+M_1_initial_camera = np.hstack((np.eye(3, 3), np.zeros((3, 1))))
+#Second camera will be at transformation [R|t]
+M_2_second_camera_location = np.hstack((R, t))
+
+#Tringulate the points
+P_l = np.dot(left_intrinsics,  M_1_initial_camera)
+P_r = np.dot(right_intrinsics,  M_2_second_camera_location)
+point_4d_hom = cv2.triangulatePoints(P_l, P_r, np.expand_dims(left_image_points, axis=1), np.expand_dims(right_image_points, axis=1))
+point_4d = point_4d_hom / np.tile(point_4d_hom[-1, :], (4, 1))
+point_3d = point_4d[:3, :].T
+point_3dx=point_3d
+
+#getting the poses for camera
+objp1 = np.float64([[0.0, 0.0, 0.0], [1.0, 0.0, 0.0], [1.0, 1.0, 0.0], [0.0, 1.0, 0.0]])
+#change the name of file accordingly to image
+cnr1= np.genfromtxt('../../parameters/left_1.csv',delimiter=',')
+cnr2= np.genfromtxt('../../parameters/left_3.csv',delimiter=',')
+
+assert cnr1.shape[0] == objp1.shape[0], 'points 3D and points 2D must have same number of vertices'
+ret1, rvec1, tvec1 = cv2.solvePnP(objp1, cnr1, left_intrinsics, dist1)
+ret2, rvec2, tvec2 = cv2.solvePnP(objp1, cnr2, left_intrinsics, dist1)
+
+tvec1 = 5 * tvec1  # Scaling
+tvec2 = 5 * tvec2  # Scaling
+
+r1, _ = cv2.Rodrigues(rvec1) #rotation vector to rotation matrix
+r2, _ = cv2.Rodrigues(rvec2) #rotation vector to rotation matrix
+
+camera_position1 = -np.matrix(r1).T * np.matrix(tvec1)
+camera_position2 = -np.matrix(r2).T * np.matrix(tvec2)
+
+# plot with matplotlib
+Ys = point_3dx[:,0]
+Zs = point_3dx[:,1]
+Xs = point_3dx[:,2]
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+ax.scatter(Xs, Ys, Zs, c='b', marker='.')
+
+plot_pyramid(ax, r1.T, camera_position1)
+plot_pyramid(ax, r2.T, camera_position2)
+
+ax.set_xlabel('Y')
+ax.set_ylabel('Z')
+ax.set_zlabel('X')
+plt.title('3D point cloud')
 plt.show()
+plt.savefig('../../output/task_7/3dpoints.png')
 
+cv2.waitKey(500)
+cv2.destroyAllWindows()
